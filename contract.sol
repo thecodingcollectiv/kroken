7400ebaad6854f31365197c0072b1311e12c205b
thecodingcollectiv <thecodingcollectiv@gmail.com>
2018-01-05 16:18:52 +0100

    added addworker, modified modifiers to include _;

--- contract.sol
+++ contract.sol
@@ -2,24 +2,23 @@ pragma solidity ^0.4.19;
 
 contract WorkOrder {
 	address public creator;
+	address public worker;
 	uint public creationTime;
 	uint public amount;
 	bool completed = false;
 	
-	modifier onlyBy(address _account)
-	{
-		require(msg.sender == _account);
-		_;
-	}
-
-	modifier notCompleted() {
-		require(!completed);
-	}
+	modifier onlyBy(address _account) { require(msg.sender == _account); _;	}
+	modifier notCompleted() { require(!completed); _; }
+	modified hasWorker() { require(worker != address(0)); _; }
 
 	function WorkOrder() public payable {
 		amount = msg.value;
 		creator = msg.sender;
 		creationTime = now;
+	}
+	
+	function addWorker(address workeraccount) public {
+		worker = workeraccount;
 	}
 	
 	function cancel() public onlyBy(creator) notCompleted {
