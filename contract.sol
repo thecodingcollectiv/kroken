c571f8fb47fe8abef2cdcfaf8fa986e3a32795b0
thecodingcollectiv <thecodingcollectiv@gmail.com>
2018-01-05 14:41:30 +0100

    update whitespaces

--- contract.sol
+++ contract.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.4.19;
 
 contract WorkOrder {
-  address public creator = new msg.sender;	
+	address public creator = new msg.sender;
 	uint public creationTime = now;
-  bool completed = false;
-  
+	bool completed = false;
+	
 	modifier onlyBy(address _account)
-  {
-      require(msg.sender == _account);
-      _;
-  }
+	{
+		require(msg.sender == _account);
+		_;
+	}
 }
