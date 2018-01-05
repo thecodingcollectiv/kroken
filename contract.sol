03ccedf3bdbc141aecbdc26c7487983fe47cd623
thecodingcollectiv <thecodingcollectiv@gmail.com>
2018-01-05 14:39:29 +0100

    update contract.sol
    the bare essentials

--- contract.sol
+++ contract.sol
@@ -1,4 +1,13 @@
 pragma solidity ^0.4.19;
 
 contract WorkOrder {
+  address public creator = new msg.sender;	
+	uint public creationTime = now;
+  bool completed = false;
+  
+	modifier onlyBy(address _account)
+  {
+      require(msg.sender == _account);
+      _;
+  }
 }
